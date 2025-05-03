9bff8b39f59364261bedf6282dfeb6175eade023
André <andre.simoesdutra23@gmail.com>
2025-05-03 19:57:30 -0300

    🔧 build(/): add dotenv dependency for environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import OpenAI from "openai";
 import simpleGit, { SimpleGit } from "simple-git";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client: OpenAI = new OpenAI({
+    apiKey: process.env.OPENAI_APIKEY as string
+});
+
+const git: SimpleGit = simpleGit();
 
 (async () => {
-    const client: OpenAI = new OpenAI();
-    const git: SimpleGit = simpleGit();
-
     
 })();
