3adc866b61c92f3abe636fcc9605e38f56d3058b
André <andre.simoesdutra23@gmail.com>
2025-05-04 14:57:42 -0300

    💎 style(simpleCommit.js): modify CLI interface UI

--- src/main.js
+++ src/main.js
@@ -9,23 +9,9 @@ program
     .name("Verus")
     .description("Verus CLI")
     .version("vrs_1.0.0")
-    .option("-c, --commit", "Make a git commit.")
-    .option("-p, --pretty", "Style the commit with emojis")
-    .action((options) => {
-        if (options.commit && options.pretty) {
-            simpleCommit();
-            return;
-        }
-
-        if (options.commit) {
-            console.log("\n- Commit comum. \n");
-            return;
-        }
-
-        if (!options.commit) {
-            console.log("\n- Acho que você está esquecendo do -c ou --commit... \n");
-            return;
-        }
+    .action(() => {
+        simpleCommit();
+        return;
     });
 
 program.parse(process.argv);
--- src/utils/simpleCommit.js
+++ src/utils/simpleCommit.js
@@ -1,23 +1,52 @@
 import simpleGit from "simple-git";
+import inquirer from "inquirer";
+import chalk from "chalk";
 
+// Initialize simpleGit to interact with the local Git repository
 const git = simpleGit();
 
+// AI Commit Method
 export async function simpleCommit() {
-    console.log("- Selecione os arquivos:\n");
-    
-    const diff = await git.diff();
-    const diffLines = diff.split("\n");
+    // Get current Git Status
+    const status = await git.status();
+    // Extract the list of modified files
+    const filesList = status.modified;
 
-    const diffList = []; 
-    
-    let oldLine = "";
-    let newLine = "";
+    // Console color styles
+    const chalkPurple = chalk.ansi256(57);
+    const chalkGrey = chalk.ansi256(238);
+    const chalkYellow = chalk.ansi256(184);
 
-    diffLines.map(line => {
-        if (line.startsWith("++") || line.startsWith("+++")) {
-            
-            
-            diffList.push()
+    // Prompt user with a checkbox to select files to commit
+    const answer = await inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'selectedFiles',
+            message: `${chalkPurple("◆")} Choose the files you want to commit:\n${chalkGrey("  ├─")} ${chalkYellow("★")}${" Tip: Use ↑ ↓ to move"}\n${chalkGrey("  │")}`,
+            prefix: '',
+            choices: filesList.map(file => ({
+                name: file,
+                value: file
+            })),
+            theme: {
+                icon: {
+                    checked: `${chalkGrey(" ├─")} ${chalkPurple("◆")}`,
+                    unchecked: `${chalkGrey(" ├─")} ${chalkPurple("◇")}`,
+                    cursor: " "
+                },
+                style: {
+                    highlight: chalk.whiteBright,
+                    disabledChoice: true
+                },
+                helpMode: 'never'
+            },
+            instructions: false
         }
-    });
+    ])
+        .catch(err => {
+            // Handle prompt errors
+            console.log(err.message);
+            console.log("\n");
+            return;
+        })
 }
